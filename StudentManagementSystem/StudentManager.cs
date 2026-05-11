a721fc8cbfda44cb20482103c585cc6b5c63f54d
NedelinGrudev <nedelingrudev12@gmail.com>
2026-05-11 17:33:25 +0300

    Implemented student manager

---
+++ StudentManagementSystem/StudentManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+    public class StudentManager
+    {
+        public List<Student> Students = new List<Student>();
+
+        public void AddStudent(Student student)
+        {
+            Students.Add(student);
+        }
+    }
+}
