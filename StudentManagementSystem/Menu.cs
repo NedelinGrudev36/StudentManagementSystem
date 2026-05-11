be513423b29b0b07778961f25ea897a82d71d895
NedelinGrudev <nedelingrudev12@gmail.com>
2026-05-11 17:47:11 +0300

    Implemented menu navigation

--- StudentManagementSystem/Menu.cs
+++ StudentManagementSystem/Menu.cs
@@ -10,9 +10,31 @@ namespace StudentManagementSystem
     {
         public void ShowMenu()
         {
-            Console.WriteLine("1. Add Student");
-            Console.WriteLine("2. Show Students");
-            Console.WriteLine("3. Exit");
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("1. Add Student");
+                Console.WriteLine("2. Show Students");
+                Console.WriteLine("3. Exit");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Add Student");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Show Students");
+                        break;
+
+                    case "3":
+                        running = false;
+                        break;
+                }
+            }
         }
     }
 }
