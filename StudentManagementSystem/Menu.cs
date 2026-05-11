04eafb546de6798e3adb6ae54a9b82352474fcdb
NedelinGrudev <nedelingrudev12@gmail.com>
2026-05-11 17:31:16 +0300

    Added menu class

---
+++ StudentManagementSystem/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementSystem
+{
+    public class Menu
+    {
+        public void ShowMenu()
+        {
+            Console.WriteLine("1. Add Student");
+            Console.WriteLine("2. Show Students");
+            Console.WriteLine("3. Exit");
+        }
+    }
+}
